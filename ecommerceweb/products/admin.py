ee7229b5f81448fed946f5c4f263741c093cd36e
Sam <samtisawesomest@gmail.com>
2020-01-31 02:07:48 +0800

    Added templates to 'products' app directory

--- ecommerceweb/products/admin.py
+++ ecommerceweb/products/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Category,Product 
 
 # Register your models here.
-admin.site.register(Item)
+admin.site.register(Category)
 admin.site.register(Product)
--- ecommerceweb/products/views.py
+++ ecommerceweb/products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
+from products.models import Product
 
-# Create your views here.
+# Displays html template
+class Home(ListView):
+    model = Product
+    template_name = 'products/home.html'
