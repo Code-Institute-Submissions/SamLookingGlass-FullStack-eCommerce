044ac2aac1f061da07ed17aedb96fd6b01dbf62f
Sam <samtisawesomest@gmail.com>
2020-01-31 13:48:22 +0800

    Added function to add product to cart

--- ecommerceweb/cart/views.py
+++ ecommerceweb/cart/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+# Function to add products to cart
+def add_to_cart(request, slug):
+    item = get_object_or_404(Product, slug=slug)
+    order_item, created = Cart.objects.get_or_create(
+        item=item,
+        user=request.user
+    )
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    
+    if order_qs.exists():
+        order = order_qs[0]
+        # Check if the product item is in the order
+        if order.orderitems.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+            messages.info(request, "This item quantity was updated.")
+            return redirect("mainapp:home")
+        else:
+            order.orderitems.add(order_item)
+            messages.info(request, "This item was added to your cart.")
+            return redirect("mainapp:home")
+    else:
+        order = Order.objects.create(
+            user=request.user)
+        order.orderitems.add(order_item)
+        messages.info(request, "This item was added to your cart.")
+        return redirect("mainapp:home")
