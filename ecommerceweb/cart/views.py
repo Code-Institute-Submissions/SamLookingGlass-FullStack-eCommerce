225e97f36cc4b7f650639bfaf5dd415837c3b621
Sam <samtisawesomest@gmail.com>
2020-01-31 13:55:19 +0800

    Added function to remove products from cart

--- ecommerceweb/cart/views.py
+++ ecommerceweb/cart/views.py
@@ -14,6 +14,7 @@ def add_to_cart(request, slug):
         order = order_qs[0]
         # Check if the product item is in the order
         if order.orderitems.filter(item__slug=item.slug).exists():
+            # Calculates quantity in cart
             order_item.quantity += 1
             order_item.save()
             messages.info(request, "This item quantity was updated.")
@@ -28,3 +29,37 @@ def add_to_cart(request, slug):
         order.orderitems.add(order_item)
         messages.info(request, "This item was added to your cart.")
         return redirect("mainapp:home")
+
+# Function to remove product from cart
+def remove_from_cart(request, slug):
+    item = get_object_or_404(Product, slug=slug)
+    cart_qs = Cart.objects.filter(user=request.user, item=item)
+    if cart_qs.exists():
+        cart = cart_qs[0]
+        # Checking the cart quantity
+        if cart.quantity > 1:
+            cart.quantity -= 1
+            cart.save()
+        else:
+            cart_qs.delete()
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        # Check if the product is in the order
+        if order.orderitems.filter(item__slug=item.slug).exists():
+            order_item = Cart.objects.filter(
+                item=item,
+                user=request.user,
+            )[0]
+            order.orderitems.remove(order_item)
+            messages.info(request, "This item was removed from your cart.")
+            return redirect("mainapp:home")
+        else:
+            messages.info(request, "This item was not in your cart")
+            return redirect("mainapp:home")
+    else:
+        messages.info(request, "You do not have an active order")
+        return redirect("core:home")
