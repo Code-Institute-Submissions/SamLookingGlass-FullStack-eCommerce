3557365a6c14640747a0499a087768ff136af4c1
Sam <samtisawesomest@gmail.com>
2020-02-10 00:53:55 +0800

    Debugged removal of item from cart function

--- ecommerceweb/cart/urls.py
+++ ecommerceweb/cart/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     # Path for cartview
     path('', CartView, name='cart'),
     # Path for decreasecart
-    path('decrease/<slug>', decreaseCart, name='decrease-cart')
+    path('decrease/<slug>', decreaseCart, name='decrease-cart'),
+    # Path for add_to_cart
+    path('add/<slug>', add_to_cart, name='add-cart'),
+    # Path for remove_from_cart
+    path('remove/<slug>', remove_from_cart, name='remove-cart'),
 
 ]
--- ecommerceweb/products/urls.py
+++ ecommerceweb/products/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
 from . views import Home
-from cart.views import add_to_cart, remove_from_cart
-from products.views import Home
 
 app_name= 'mainapp'
 
 urlpatterns = [
+    # Path for homepage
     path('', Home.as_view(), name='home'),
-    # Adding new paths for cart
-    path('cart/<slug>', add_to_cart, name='cart'),
-    path('remove/<slug>', remove_from_cart, name='remove-cart'),
 ]
