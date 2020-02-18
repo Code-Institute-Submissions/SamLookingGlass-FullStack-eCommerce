b6fa40155c94d75647d34d8ed67baca7486980f7
Sam <samtisawesomest@gmail.com>
2020-02-19 04:52:37 +0800

    Added static root in settings.py

--- ecommerceweb/settings.py
+++ ecommerceweb/settings.py
@@ -144,7 +144,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Directory path for uploaded media
 MEDIA_URL = '/media/'
